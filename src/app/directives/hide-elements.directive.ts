f1d4b49e4920d83e759106ad11ce90f882da6eed
Allan <allanrodrigol@gmail.com>
2020-04-23 14:18:02 -0300

    Bug fix role guard and hide directive

--- src/app/directives/hide-elements.directive.ts
+++ src/app/directives/hide-elements.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import { LoginService } from '../services/login.service';
 
 @Directive({
-  // tslint:disable-next-line: directive-selector
   selector: '[RoleVerification]'
 })
-export class HideElementsDirective {
-  @Input() roles: string[] = [];
+export class HideElementsDirective implements OnInit {
+  @Input("RoleVerification") roles: string[] = [];
   
   constructor(private elm: ElementRef, private loginService: LoginService) {
+  }
+
+  ngOnInit() {
     if (!this.roles.includes(this.loginService.getUserRole())) {
       this.elm.nativeElement.style.display = 'none';
     }
